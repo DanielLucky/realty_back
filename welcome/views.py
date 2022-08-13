c682351a24d4b6710da05074304bd6ee9f8a44e1
dl <danil.spb.it@gmail.com>
2022-08-13 20:46:32 +0300

    Новый сериализатор для модели realty

---
+++ welcome/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def welcome(request):
+    return Response({'title': 'Welcome to RealtySell. Please check Documentation `https://documenter.getpostman.com/view/17461733/UzkS3HZz`'}, status=200)
